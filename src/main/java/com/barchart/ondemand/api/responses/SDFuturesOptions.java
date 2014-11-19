a7a38539f1f1fb5aad3d4eeae67051ab8fe741cd
mvberg <mvberg@gmail.com>
2014-11-19 14:19:50 -0600

    added convenience methods for working with ShortDatedFuturesOptions

--- src/main/java/com/barchart/ondemand/api/responses/SDFuturesOptions.java
+++ src/main/java/com/barchart/ondemand/api/responses/SDFuturesOptions.java
@@ -3,7 +3,9 @@ package com.barchart.ondemand.api.responses;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
@@ -38,6 +40,50 @@ public class SDFuturesOptions extends ResponseBase {
 			if (f.getType() != null && f.getType().equalsIgnoreCase("put")) {
 				results.add(f);
 			}
+		}
+
+		return results;
+	}
+
+	public Collection<SDFuturesOption> forContractMonth(final String month) {
+		final List<SDFuturesOption> results = new ArrayList<SDFuturesOption>();
+
+		for (SDFuturesOption f : all()) {
+			if (f.getContractMonth().equalsIgnoreCase(month)) {
+				results.add(f);
+			}
+		}
+
+		return results;
+	}
+
+	public Collection<String> allExpirations() {
+		final List<String> results = new ArrayList<String>();
+
+		for (SDFuturesOption f : all()) {
+			if (!results.contains(f.getExpirationDate())) {
+				results.add(f.getExpirationDate());
+			}
+		}
+
+		return results;
+	}
+
+	public Map<String, SDFuturesOptions> expirationsForMonth(final String month) {
+		final Map<String, SDFuturesOptions> results = new HashMap<String, SDFuturesOptions>();
+
+		final Collection<SDFuturesOption> all = forContractMonth(month);
+
+		if (all == null) {
+			return new HashMap<String, SDFuturesOptions>();
+		}
+
+		for (SDFuturesOption a : all) {
+			if (results.get(a.getExpirationDate()) == null) {
+				results.put(a.getExpirationDate(), new SDFuturesOptions());
+			}
+
+			results.get(a.getExpirationDate()).results.add(a);
 		}
 
 		return results;
